644bf29ea06a9d7408ead96a836b00b51f17e0c9
Daniel Stöhr <stoehr-daniel@web.de>
2022-09-26 22:06:12 +0200

    index page finished (after 2 hours)

--- knowledge/views.py
+++ knowledge/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 
 from knowledge.models import KnowlegeBase
+from django.db.models import Q
 
 # Create your views here.
 
 
 def start(request):
     if request.method == "POST":
-        print("trying to post")
-    posts = KnowlegeBase.objects.all()
-    data = {"posts": posts}
+        search = request.POST["search"]
+        posts = KnowlegeBase.objects.filter(
+            Q(title__contains=search) | Q(text__contains=search)
+        )
+    else:
+        search = ""
+        posts = KnowlegeBase.objects.all()
+    data = {"posts": posts, "search": search}
     return render(request, "index.html", data)
 
 
